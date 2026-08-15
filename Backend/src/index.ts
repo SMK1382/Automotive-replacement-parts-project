f119fffcde7c3917844a7d9856ea8a9078211dda
Mohammadreza_Kalali <kalalimohammad1382@gmail.com>
2026-08-16 01:28:00 +0330

    2026-8-16

---
+++ Backend/src/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+app.use(express.json())
+
+app.get('/', (req, res) =>{
+    res.json({'message' : 'server running'})
+})
+
+
+const Port = Number(process.env.Port) || 4000
+
+app.listen(Port, () =>{
+    console.log(`Start server on port ${Port}`);
+})
